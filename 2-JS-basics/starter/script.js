86f50f30d7c2ff3f506fd2a2871c71225c9c2c5e
Micael Torp <micael@wico.one>
2018-12-22 11:43:49 +0100

    egen bmi calculator

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -20,6 +20,22 @@ Variabels mutations and typos
 
 */
 
+/*
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+*/
+
+/*
 var  firstName = 'John';
 var age = 56;
 
@@ -31,9 +47,45 @@ ismarriage = false;
 
 alert(firstName);
 
-var lastName = prompt('What is hes lastname');
+var lastName = prompt('What is hes lastname of john');
 
 console.log(firstName + ' ' + lastName);
+*/
+
+/*
+//John
+var massJohn = 78;
+var heightJohn = 1.96;
+
+//Mark
+var massMark = 98;
+var heightMark = 1.94;
+
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+*/
+//Bmi Calculator
+
+var massYour = prompt('Hvad er din vægt?');
+var heightYour = prompt('Hvad er din højde?');
+
+
+var BMIdin = massYour / (heightYour * heightYour);
+
+//console.log(BMIdin);
+var BMIdin = alert('Din bmi er' + ' ' + BMIdin);
+
+
+
+
+
 
 
 
