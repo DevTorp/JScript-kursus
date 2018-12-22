9c36adeaf46d4fef3c54e4ee1000fa987c78008b
Micael Torp <45742238+DevTorp@users.noreply.github.com>
2018-12-22 11:05:20 +0100

    Første Upload

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,41 @@
+/***********************
+Variabels and Data types
+
+*/
+/*
+var firstName = 'John';
+var lastName = 'Andersen';
+var age = 45;
+var job;
+job = 'Teacher';
+var job2;
+
+//Konsol visning
+console.log(firstName + ' ' + lastName + ' ' + age);
+console.log(job);
+console.log(job2);
+*/
+/***********************
+Variabels mutations and typos
+
+*/
+
+var  firstName = 'John';
+var age = 56;
+
+console.log(firstName + ' ' + age);
+
+var job, ismarriage;
+
+ismarriage = false;
+
+alert(firstName);
+
+var lastName = prompt('What is hes lastname');
+
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
